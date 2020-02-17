900cd7a55475984183e11630cf340139c705ab71
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-02-17 16:56:05 +0000

    part 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,15 @@
 require 'docking_station'
 
 describe DockingStation do
-  # docking_station = DockingStation.new
-  # it "can release a bike" do
-  #   expect(docking_station).to respond_to(:release_bike)
-  # end
-
   it {is_expected.to respond_to(:release_bike)}
+  
+  docking_station = DockingStation.new
+  it "creates a bike" do
+    expect(docking_station.release_bike).to be_instance_of(Bike)
+  end
 
+  bike = docking_station.release_bike
+  it "new bike works" do
+    expect(bike.working?).to eq(true)
+  end
 end

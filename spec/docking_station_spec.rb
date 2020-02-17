e13da1db31af47a2714e889e063403f809469f29
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-02-17 15:54:44 +0000

    release bike test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
-  
+  # docking_station = DockingStation.new
+  # it "can release a bike" do
+  #   expect(docking_station).to respond_to(:release_bike)
+  # end
+
+  it {is_expected.to respond_to(:release_bike)}
+
 end
